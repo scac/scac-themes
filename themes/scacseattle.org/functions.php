fa174a8db38c00d78cb44cf0da38c0a3c26930bd
Justin Louie <justin@cmn.com>
2012-12-03 21:42:04 -0800

    chagned social buttons back, added search mag

--- themes/scacseattle.org/functions.php
+++ themes/scacseattle.org/functions.php
@@ -6,6 +6,7 @@ wp_enqueue_script('main', get_stylesheet_directory_uri() . "/js/main.js", array(
 wp_enqueue_script('flexslider', get_stylesheet_directory_uri() . "/js/jquery.flexslider-min.js", array("jquery"));
 
 //CSS
+wp_enqueue_style('webfonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700', null, null, null);
 wp_enqueue_style( 'flexslider-css', get_stylesheet_directory_uri() . '/flexslider.css', null, null, null);
 
 ?>
