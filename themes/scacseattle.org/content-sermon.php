5600c7943faa69ea909f6cb8466527de84b3788d
Justin Louie <justin@cmn.com>
2012-12-14 00:50:14 -0800

    added the color into nav

--- themes/scacseattle.org/content-sermon.php
+++ themes/scacseattle.org/content-sermon.php
@@ -5,12 +5,37 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class("sermon-post"); ?>>
   <header class="entry-header">
     <h1 class="entry-title"><?php the_title(); ?></h1>
 
-    <div class="entry-meta">
-      <?php _s_posted_on(); ?>
+    <div class="entry-meta"> 
+      <?php if (get_post_type() == 'ct_sermon') : ?>
+        <span class="sermon-speaker">
+          <?php echo get_the_term_list($post->ID, 'sermon_speaker'); ?>,
+        </span>
+      <?php endif; ?>
+
+      <?php the_time('M j, Y'); ?>
+      
+      <div class="comments-cnt">
+        <a href="<?php comments_link(); ?>" class="comments-link">
+        <?php 
+        $ccnt = get_comments_number();
+        switch($ccnt){
+          case 0:
+            echo "No Comments";
+            break;
+          case 1:
+            echo "1 Comment";
+            break;
+          default:
+            echo "$ccnt Comments";
+            break;
+        }
+        ?>
+        </a>
+      </div>
     </div><!-- .entry-meta -->
   </header><!-- .entry-header -->
 
--- themes/scacseattle.org/navbar.php
+++ themes/scacseattle.org/navbar.php
@@ -1,19 +1,31 @@
-<!-- <nav id="primary-nav" role="navigation" class="site-navigation main-navigation  black-bar">
-  <ul class="navbar clearfix">
-    <li class="nav-item disabled"><a href="#">Church</a></li>
-    <li class="nav-item"><a href="<?php echo home_url("/visit/"); ?>">Visit</a></li>
-    <li class="nav-item disabled"><a href="#">Ministries</a></li>
-    <li class="nav-item disabled"><a href="#">Sermons</a></li>
-    <li class="nav-item disabled"><a href="#">Give</a></li>
-  </ul>
-</nav> --><!-- .site-navigation .main-navigation -->
+<?php
+global $post;
+$church_page = get_page_by_title('church')
+?>
 
-<table  id="primary-nav" role="navigation" class="site-navigation main-navigation  black-bar">
-  <tr class="navbar clearfix">
-    <td class="nav-item"><a href="<?php echo home_url("/church/our-vision/"); ?>">Church</a></td>
-    <td class="nav-item"><a href="<?php echo home_url("/visit/"); ?>">Visit</a></td>
-    <td class="nav-item disabled"><a href="#">Ministries</a></td>
-    <td class="nav-item"><a href="<?php echo home_url("/sermons/"); ?>">Sermons</a></td>
-    <td class="nav-item disabled"><a href="#">Give</a></td>
-  </tr>
-</table>
+<div class="primary-nav-container">
+  <table  id="primary-nav" role="navigation" class="site-navigation main-navigation  black-bar">
+    <tr class="navbar clearfix">
+      <td class="nav-item"><a href="<?php echo home_url("/church/our-vision/"); ?>">
+        <?php if(is_page('church') || $post->post_parent == $church_page->ID) : ?>
+          <span class="active_tab"></span>
+        <?php endif; ?>
+        Church</a>
+      </td>
+      <td class="nav-item"><a href="<?php echo home_url("/visit/"); ?>">
+        <?php if(is_page('visit')) : ?>
+          <span class="active_tab"></span>
+        <?php endif; ?>
+        Visit</a>
+      </td>
+      <td class="nav-item disabled"><a href="#">Ministries</a></td>
+      <td class="nav-item"><a href="<?php echo home_url("/sermons/"); ?>">
+        <?php if(get_post_type() == 'ct_sermon') : ?>
+          <span class="active_tab"></span>
+        <?php endif; ?>
+        Sermons</a>
+      </td>
+      <td class="nav-item disabled"><a href="#">Give</a></td>
+    </tr>
+  </table>
+</div>
