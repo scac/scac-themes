1e0826f06f825c876cc0e9901dd7983582411f50
Justin Louie <justin@cmn.com>
2012-12-19 23:16:38 -0800

    added latest sermon to homepage

--- themes/scacseattle.org/page-homepage.php
+++ themes/scacseattle.org/page-homepage.php
@@ -20,31 +20,58 @@ get_header(); ?>
           </tr>
           <tr class="info-row">
             <td class="info-box info-cell">
-              <table>
-                <tr>
-                  <td class="service-time">8:30am</td>
-                  <td>Cantonese Service 1</td>
-                </tr>
-                <tr>
-                  <td class="service-time">10:00am</td>
-                  <td>English Ministries Service</td>
-                </tr>
-                <tr>
-                  <td class="service-time">11:30am</td>
-                  <td>Cantonese Service 2</td>
-                </tr>
-              </table>
+              <div class="info-cell-container">
+                <table>
+                  <tr>
+                    <td class="service-time">8:30am</td>
+                    <td>Cantonese Service 1</td>
+                  </tr>
+                  <tr>
+                    <td class="service-time">10:00am</td>
+                    <td>English Ministries Service</td>
+                  </tr>
+                  <tr>
+                    <td class="service-time">11:30am</td>
+                    <td>Cantonese Service 2</td>
+                  </tr>
+                </table>
 
-              <p>
-                2803 S. Orcas St.
-                <br/>
-                Seattle, WA 98108
-              </p>
+                <p>
+                  2803 S. Orcas St.
+                  <br/>
+                  Seattle, WA 98108
+                </p>
 
-              <a href="<?php echo home_url("/")?>visit/">Directions</a>
+                <a href="<?php echo home_url("/")?>visit/">Directions</a>
+              </div>
             </td>
             <td class="info-box info-cell">
-              Coming Soon
+              <?php
+                $args = array(
+                  'post_type' => 'ct_sermon',
+                  'order' => 'DESC',
+                  'numberposts' => 1
+                );
+                $post = array_shift(get_posts($args));
+
+                if(isset($post)) :                  
+                  $term = array_shift(wp_get_post_terms($post->ID, 'sermon_series'));
+
+              ?>
+                <div class="info-cell-container">
+                  <?php 
+                    global $nggdb;
+                    if(isset($nggdb)):
+                      $img = $nggdb->find_image($term->slug);
+                    ?>
+                      <div class="latest-sermon-img"><img src="<?php echo $img->imageURL; ?>" style="width: 300px"></div>
+                    <?php endif; ?>
+                  <div class="latest-sermon-title"><a href="<?php echo get_permalink($post); ?>"><?php echo get_the_title($post); ?></a></div>
+                  <div class="latset-sermon-date"><?php echo get_the_time('M j, Y', $post); ?></div>
+                </div>
+              <?php else : ?>
+                Latest sermon coming soon! Check often!
+              <?php endif; ?>
             </td>
             <td class="info-box info-cell">
               <?php
